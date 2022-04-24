778c6a971ab6dc7239abd0bf5363a22ba00771bf
Volodymyr <vthehang@gmail.com>
2022-04-24 16:30:43 +0300

    added possibility to add users from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { CREATE_USER } from "./mutation/user";
 import { GET_ALL_USERS } from "./query/user";
 
 const App = () => {
     const { data, loading, error } = useQuery(GET_ALL_USERS);
+    const [newUser] = useMutation(CREATE_USER);
     const [users, setUsers] = useState([]);
+    const [username, setUsername] = useState("");
+    const [age, setAge] = useState(0);
+
     useEffect(() => {
         if (!loading) {
             setUsers(data.getAllUsers);
         }
     }, [data]);
+
+    const addUser = (e) => {
+        e.preventDefault();
+        newUser({
+            variables: {
+                input: {
+                    username,
+                    age,
+                },
+            },
+        }).then(({ data }) => {
+            console.log(data);
+            setUsername("");
+            setAge(0);
+        });
+    };
 
     if (loading) {
         return <h1>Loading...</h1>;
@@ -19,10 +40,18 @@ const App = () => {
     return (
         <div>
             <form>
-                <input type="text" />
-                <input type="number" />
+                <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                />
+                <input
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    type="number"
+                />
                 <div className="btns">
-                    <button>Create</button>
+                    <button onClick={(e) => addUser(e)}>Create</button>
                     <button>Get</button>
                 </div>
             </form>
